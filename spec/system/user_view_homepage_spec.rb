742616cb91a7c8d069eae7e99b89075fa29471cf
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-11-29 23:36:03 -0300

    Updating user home page view test

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -11,4 +11,22 @@ describe 'User viewing homepage' do
     # Assert
     expect(page).to have_content('Welcome to AutoWarehousePro!')
   end
+
+  it 'and sees Visitor viewing warehouses' do
+    # Arrange
+    Warehouse.create(name: 'Galpão BH', code: 'SDU', area: 40_000, city: 'Belo Horizonte')
+    Warehouse.create(name: 'Galpão SC', code: 'GIG', area: 60_000, city: 'Santa Catarina')
+    # Act
+    visit root_path
+    # Assert
+    expect(page).to have_content('Galpão BH')
+    expect(page).to have_content('Código do Aeroporto: SDU')
+    expect(page).to have_content(' Área: 40,000m²')
+    expect(page).to have_content('Cidade: Belo Horizonte')
+
+    expect(page).to have_content('Galpão SC')
+    expect(page).to have_content('Código do Aeroporto: GIG')
+    expect(page).to have_content(' Área: 80,000m²')
+    expect(page).to have_content('Cidade: Santa Catarina')
+  end
 end
