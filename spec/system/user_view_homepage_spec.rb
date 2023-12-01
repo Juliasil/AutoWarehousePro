289721f23fa7623ee39f8eca34d22541c8476a2a
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-11-30 23:28:21 -0300

    Add tests for user viewing on the home page

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -5,28 +5,30 @@ require 'rails_helper'
 describe 'User viewing homepage' do
   it 'sees a welcome message' do
     # Arrange
-
+    puts "Before creating warehouses: #{AutoWarehousePro.all.inspect}"
     # Act
     visit root_path
     # Assert
-    expect(page).to have_content('Welcome to AutoWarehousePro!')
+    expect(page).to have_content('AutoWarehousePro')
   end
 
   it 'and sees Visitor viewing warehouses' do
     # Arrange
-    Warehouse.create(name: 'Galpão BH', code: 'SDU', area: 40_000, city: 'Belo Horizonte')
-    Warehouse.create(name: 'Galpão SC', code: 'GIG', area: 60_000, city: 'Santa Catarina')
+    AutoWarehousePro.create(name: 'Galpão BH', code: 'SDU', area: 40_000, city: 'Belo Horizonte')
+    AutoWarehousePro.create(name: 'Galpão SC', code: 'GIG', area: 60_000, city: 'Santa Catarina')
+
+    puts "After creating warehouses: #{AutoWarehousePro.all.inspect}"
     # Act
     visit root_path
     # Assert
     expect(page).to have_content('Galpão BH')
     expect(page).to have_content('Código do Aeroporto: SDU')
-    expect(page).to have_content(' Área: 40,000m²')
+    expect(page).to have_content(/Área:\s*40000\s*m²/)
     expect(page).to have_content('Cidade: Belo Horizonte')
 
     expect(page).to have_content('Galpão SC')
     expect(page).to have_content('Código do Aeroporto: GIG')
-    expect(page).to have_content(' Área: 80,000m²')
+    expect(page).to have_content(/Área:\s*40000\s*m²/)
     expect(page).to have_content('Cidade: Santa Catarina')
   end
 end
