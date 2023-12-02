0fe8b5153feca498ac47156fd3f3474ccfbdf425
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2023-12-02 20:38:28 -0300

    Enhance test coverage in user_view_homepage_spec.rb

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 describe 'User viewing homepage' do
   it 'sees a welcome message' do
     # Arrange
-    puts "Before creating warehouses: #{AutoWarehousePro.all.inspect}"
     # Act
     visit root_path
     # Assert
@@ -17,10 +16,10 @@ describe 'User viewing homepage' do
     AutoWarehousePro.create(name: 'Galpão BH', code: 'SDU', area: 40_000, city: 'Belo Horizonte')
     AutoWarehousePro.create(name: 'Galpão SC', code: 'GIG', area: 60_000, city: 'Santa Catarina')
 
-    puts "After creating warehouses: #{AutoWarehousePro.all.inspect}"
     # Act
     visit root_path
     # Assert
+    expect(page).not_to have_content('There are no registered warehouses')
     expect(page).to have_content('Galpão BH')
     expect(page).to have_content('Código do Aeroporto: SDU')
     expect(page).to have_content(/Área:\s*40000\s*m²/)
@@ -30,5 +29,14 @@ describe 'User viewing homepage' do
     expect(page).to have_content('Código do Aeroporto: GIG')
     expect(page).to have_content(/Área:\s*40000\s*m²/)
     expect(page).to have_content('Cidade: Santa Catarina')
+  end
+
+  it 'and there is no registered warehouse' do
+    # Arrange
+
+    # Act
+    visit root_path
+    # Assert
+    expect(page).to have_content('There are no registered warehouses')
   end
 end
